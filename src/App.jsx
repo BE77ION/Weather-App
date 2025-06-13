89b7bea1e81b71fde3615b693307db83f4d118e6
BE77ION <pawandeepsingh7897@gmail.com>
2025-06-13 21:10:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Weather from './components/Weather'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='App'>
+      <Weather></Weather>
+      </div>
+    </>
+  )
+}
+
+export default App
